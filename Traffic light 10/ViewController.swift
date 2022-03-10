369665ced31af03b6ef2793cc5f3007db31600f2
Sosina Tatyana <fedorova_t95@mail.ru>
2022-03-10 12:50:33 +0300

    Initial Commit

---
+++ Traffic light 10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Traffic light 10
+//
+//  Created by Tatiana Sosina on 10.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
