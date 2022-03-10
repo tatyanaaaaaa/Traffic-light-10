580c79521169e077f8fab9221822d0a5b5aa9a55
Sosina Tatyana <fedorova_t95@mail.ru>
2022-03-10 13:19:22 +0300

    Add settings traffic light

--- Traffic light 10/ViewController.swift
+++ Traffic light 10/ViewController.swift
@@ -7,13 +7,69 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+final class ViewController: UIViewController {
+    enum ColorTrafficLight {
+        case red
+        case yellow
+        case green
     }
 
+    // MARK: - Private property
+    
+    @IBOutlet weak private var trafficLightView: UIView!
+    @IBOutlet weak private var redColorTrafficLightView: UIView!
+    @IBOutlet weak private var yellowColorTrafficLightView: UIView!
+    @IBOutlet weak private var greenColorTrafficLightView: UIView!
+    @IBOutlet weak private var trafficLightButton: UIButton!
+    
+    private var colorTrafficLight: ColorTrafficLight = .red
+    
+    // MARK: - Internal func
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setDefaultSettings()
+    }
 
+    // MARK: - Private func
+    
+    @IBAction private func switchTrafficLightButton() {
+        switchColorsTrafficLight()
+    }
+    
+    private func setDefaultSettings() {
+        trafficLightView.layer.cornerRadius = 22
+        
+        redColorTrafficLightView.layer.cornerRadius = redColorTrafficLightView.frame.height / 1.7
+        redColorTrafficLightView.backgroundColor = .red.withAlphaComponent(0.3)
+        
+        yellowColorTrafficLightView.layer.cornerRadius = yellowColorTrafficLightView.frame.height / 1.7
+        yellowColorTrafficLightView.backgroundColor = .yellow.withAlphaComponent(0.3)
+        
+        greenColorTrafficLightView.layer.cornerRadius = greenColorTrafficLightView.frame.height / 1.7
+        greenColorTrafficLightView.backgroundColor = .green.withAlphaComponent(0.3)
+        
+        trafficLightButton.layer.cornerRadius = 19
+        trafficLightButton.setTitle("Start", for: .normal)
+    }
+    
+    private func switchColorsTrafficLight() {
+        trafficLightButton.setTitle("Next", for: .normal)
+        
+        switch colorTrafficLight {
+        case .red:
+            greenColorTrafficLightView.backgroundColor = .green.withAlphaComponent(0.3)
+            redColorTrafficLightView.backgroundColor = .red
+            colorTrafficLight = .yellow
+        case .yellow:
+            yellowColorTrafficLightView.backgroundColor = .yellow
+            redColorTrafficLightView.backgroundColor = .red.withAlphaComponent(0.3)
+            colorTrafficLight = .green
+        case .green:
+            greenColorTrafficLightView.backgroundColor = .green
+            yellowColorTrafficLightView.backgroundColor = .yellow.withAlphaComponent(0.3)
+            colorTrafficLight = .red
+        }
+    }
 }
 
